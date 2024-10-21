6cf45d562b506ae47f8b016f05f7a7d46064e14f
Muthukrishnan Kandasamy <muthukrishnan.kandasamy@syncfusion.com>
2024-10-21 17:58:47 +0530

    Updated react scheduler sample to latest version.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { registerLicense } from '@syncfusion/ej2-base';
 
-registerLicense('ORg4AjUWIQA/Gnt2VVhhQlFaclhJWHxMYVF2R2FJeFRycF9FaEwgOX1dQl9hSXpTcEVmWn9feHVRQWY=');
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
